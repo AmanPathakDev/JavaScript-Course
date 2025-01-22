246c4a382adcc9c9df2c36a090055488fd785ece
Aman Pathak <124494986+AmanPathakDev@users.noreply.github.com>
2025-01-22 19:23:57 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 123323
+let accountEmail = "aman@gmail.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+let accountState;
+
+// accountId = 2 // Not Allowed
+
+accountEmail = "abc@gmail.com"
+accountPassword = "45678"
+accountCity = "Pune"
+
+// console.log(accountId);
+
+/*
+Prefer not to use Var
+because of block scope and functional scope
+*/
+
+// console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3 ) // we are using NodeJs not browser
+
+let name = "Aman"
+let number = 123
+let isLoggedIn = false
+
+
+// number range is 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbols => unique
+
+// object
+
+console.log(typeof undefined);  // undefined
+console.log(typeof null); // object
