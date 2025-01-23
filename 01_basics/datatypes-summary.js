c0d1225dc8f1bcd55bdb2bcd850926a44041472d
Aman Pathak <124494986+AmanPathakDev@users.noreply.github.com>
2025-01-23 18:25:43 +0000

    Non premetive heap

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -34,3 +34,25 @@ const myFunction = function(){
 }
 
 console.log(typeof bigNumber)
+
+// *********************************************************************
+
+let myYoutubeName = "hiteshchoudharydotcom"
+
+let anotherName = myYoutubeName
+anotherName = "chaiaurcode"
+
+console.log(myYoutubeName)
+console.log(anotherName)
+
+let userOne = {
+    email: "aman@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "aman@gmail.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
