96b85196a8032779ebf39c53bb8e6f862acef5a2
Aman Pathak <124494986+AmanPathakDev@users.noreply.github.com>
2025-01-25 17:47:06 +0000

    Object p2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,22 @@
+// const tinderUser = new Object()
+
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sam"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser)
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {4: "a", 5: "b"}
+
+// const obj3 = {obj1, obj2}
+
+// const obj3 = Object.assign({}, obj1, obj2)
+const obj3 = {...obj1, ...obj2}
+
+console.log(obj3)
+
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser))
