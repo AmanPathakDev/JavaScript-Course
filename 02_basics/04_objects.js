90c18a81874cf5e116566748ebfb936e5ae96273
Aman Pathak <124494986+AmanPathakDev@users.noreply.github.com>
2025-01-25 17:58:57 +0000

    json talk

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -16,7 +16,18 @@ const obj2 = {4: "a", 5: "b"}
 // const obj3 = Object.assign({}, obj1, obj2)
 const obj3 = {...obj1, ...obj2}
 
-console.log(obj3)
+// console.log(obj3)
 
-console.log(Object.keys(tinderUser))
-console.log(Object.values(tinderUser))
+// console.log(Object.keys(tinderUser))
+// console.log(Object.values(tinderUser))
+
+
+const course = {
+    courseName: "JS in hindi",
+    price: 999,
+    courseTeacher: "Aman"
+}
+
+const {courseTeacher: teacher} = course
+
+console.log(teacher)
