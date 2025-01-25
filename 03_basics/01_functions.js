f5f6b9545ebdf6f90f9119e617b5b58dda5bac5e
Aman Pathak <124494986+AmanPathakDev@users.noreply.github.com>
2025-01-25 19:32:28 +0000

    Function p2

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -32,4 +32,34 @@ function loginUserMessage(username = "Sam") {
     return `${username} logged in`
 }
 
-console.log(loginUserMessage())
+// console.log(loginUserMessage())
+
+function calcCartPrice(...num) {
+    return num
+}
+
+// console.log(calcCartPrice(100, 200, 300, 500))
+
+const user = {
+    username: "Aman",
+    price: 33
+}
+
+function handelObject(anyObject) {
+    return `Username is ${anyObject.username} and price is ${anyObject.price}`
+}
+
+// console.log(handelObject(user))
+console.log(handelObject({
+    username: "sam",
+    price: 55
+}))
+
+
+const myNewArray = [200, 400, 600]
+
+function returnSecondArray(myArray) {
+    return myArray[2]
+}
+
+console.log(returnSecondArray(myNewArray))
