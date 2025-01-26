2e13f4cdbefcf554fb6d4df6030ab532f67a44d5
Aman Pathak <124494986+AmanPathakDev@users.noreply.github.com>
2025-01-26 15:32:46 +0000

    scope p2

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,6 +6,47 @@ if(true) {
     const b = 20
 }
 
-console.log(a)
+// console.log(a)
 // console.log(b)
 // console.log(c)
+
+
+function one() {
+    const username = "Aman"
+
+    function two() {
+        const website = "youtube"
+        console.log(username)
+    }
+    // console.log(website)
+
+    two()
+}
+
+// one()
+
+if (true) {
+    const username = "Aman"
+
+    if(username === "Aman") {
+        const website = " youtube"
+        console.log(username + website)
+    }
+    // console.log(website)
+}
+
+// console.log(username)
+
+
+// ****************************** Interesting **************************************8
+
+addone(5)
+function addone(num) {
+    return num + 1
+}
+
+
+addTwo(3)
+const addTwo = function(num) {
+    return num + 1
+}
