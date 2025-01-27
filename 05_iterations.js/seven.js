ea914a72da87544d08b9366e0b5fd4b5610ccfa3
Aman Pathak <124494986+AmanPathakDev@users.noreply.github.com>
2025-01-27 16:07:36 +0000

    map, reduce, filter

---
+++ 05_iterations.js/seven.js
@@ -0,0 +1,10 @@
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumers.map((item) => item + 10)
+
+const newNums = myNumers
+    .map((item) => item * 10)
+    .map((item) => item + 1)
+    .filter((item) => item >= 40)
+
+console.log(newNums)
